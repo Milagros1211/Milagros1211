648a0afefb223fe31b689770caf63dcf0c981136
Milagros1211 <a20171272@pucp.edu.pe>
2024-11-27 06:52:33 -0500

    commit

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
+# Solicitar al usuario que ingrese datos
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+ciudad = input("Ingrese su ciudad: ")
 
+# Imprimir los datos ingresados
+print("\nDatos ingresados:")
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Ciudad:", ciudad)
