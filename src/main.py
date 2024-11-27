a6d58e987a839ba0649b79539493411bc3e99d40
Milagros1211 <a20171272@pucp.edu.pe>
2024-11-27 07:16:18 -0500

    commit

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 import datetime
 
+# Modo de uso de software
+Operation_type = input("¿Que tipo de operacion quiere realizar?")
+
+match Operation_type:
+    case 'consulta':
+        time_interval = input("Tipo de bolsa de carbon: ")
+    case 'ingreso':
+        time_interval = input("Tipo de bolsa de carbon: ")
+    case _:
+        print("Operacion incorrecta")
+
 # Solicitar al usuario que ingrese datos
 bag_type = input("Tipo de bolsa de carbon: ")
 carbon_type = input("Tipo de carbon: ")
 volume = input("Ingrese el volumen ingresado: ")
-timestamp = datetime.datetime.now()
+
+# Obtener la fecha y hora actual y formatearla hasta los segundos
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # Imprimir los datos ingresados
 print("\nDatos ingresados:")
